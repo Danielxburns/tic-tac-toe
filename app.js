78288df7ce3ddda723704b3dceae41a8c6255917
Danielxburns <danielburnsart@gmail.com>
2019-03-18 17:03:13 -0500

    places an X in each square on click

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('App loaded!');
+
+var board = document.getElementById("board");
+
+var placed = board.addEventListener('click', (e) => {
+  console.log(e.target.parentNode.className, e.target.className);
+  e.target.innerHTML = 'X';
+
+});
+
+// need to place a piece when a square is clicked
+// alternate piece after each turn
+// make that piece unclickable
+// update turn field
