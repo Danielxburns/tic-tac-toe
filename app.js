638eee80a1a37000a2bc0d93d225a78d5dd35a8b
Danielxburns <danielburnsart@gmail.com>
2019-03-18 17:47:22 -0500

    alternates between X and O and updates who's turn it is

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ console.log('App loaded!');
 
 var board = document.getElementById("board");
 
-var placed = board.addEventListener('click', (e) => {
-  console.log(e.target.parentNode.className, e.target.className);
-  e.target.innerHTML = 'X';
-
+// need to place a piece when a square is clicked
+var square = board.addEventListener('click', (e) => {
+  var sq = e.target;
+  var row = e.target.parentNode;
+  console.log(row.className, sq.className);
+  piece(sq);
+  // return (sq);
+  // e.target.innerHTML = 'X';
 });
 
-// need to place a piece when a square is clicked
 // alternate piece after each turn
-// make that piece unclickable
 // update turn field
+var xTurn = true;
+var piece = function(sq) {
+  if (!sq.innerHTML) {
+    if (xTurn) {
+      sq.innerHTML = 'X';
+      xTurn = !xTurn;
+      document.getElementById('turn').innerHTML=('');
+      document.getElementById('turn').innerHTML=('O\'s turn')
+    } else {
+      sq.innerHTML = 'O';
+      xTurn = !xTurn;
+      document.getElementById('turn').innerHTML=('');
+      document.getElementById('turn').innerHTML=('X\'s turn')
+    }
+  }
+};
+
+// reset game when play again button is clicked
+// keep track of game play and implement rules
