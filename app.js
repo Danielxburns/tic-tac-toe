73050419b44553bcd6790d16b9edaa5ffc2f48a0
Danielxburns <danielburnsart@gmail.com>
2019-03-18 22:24:01 -0500

    complete

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-console.log('App loaded!');
-
 var board = document.getElementById("board");
 
-// need to place a piece when a square is clicked
+// Event listener
 var square = board.addEventListener('click', (e) => {
   var sq = e.target;
   var row = e.target.parentNode;
@@ -35,26 +33,34 @@ var colTally = function(moves) {
 
 var checker = function(tally) {
   for (key in tally) {
-    // console.log('checker tally ' + tally);
     if (tally[key] === 3) {
       return true;
     }
   }
 }
+// after every move
+// looop thru moves and if
+var diagonalChecker = function(moves) {
+  var majorDia = moves.filter((move) => move.toString() === ['A', '1'].toString() || ['B', '2'].toString() || ['C', '3'].toString());
+  var minorDia = moves.filter((move) => move.toString() === ['C', '1'].toString() || ['B', '2'].toString() || ['A', '3'].toString())
+  if ((majorDia.toString() === [['A', '1'], ['B', '2'], ['C', '3']].toString()) || (minorDia.toString() === [['C', '1'], ['B', '2'], ['A', '3']].toString())) {
+    return true;
+  }
+};
 
+// keep track of game play
 var xTurn = true;
 var xMoves = [];
 var oMoves = [];
 
-// alternate piece after each turn and updates turn field
+// alternates piece after each turn, checks for a winner and updates status message
 var piece = function(row,sq) {
   if (!sq.innerHTML) {
     if (xTurn) {
       sq.innerHTML = 'X';
       xMoves.push([row.className, sq.className]);
-      // console.log(checker(rowTally(xMoves)));
       //if there are X's in any three rows or any three cols then x is the winner
-      if (checker(rowTally(xMoves)) || checker(colTally(xMoves))) {
+      if (checker(rowTally(xMoves)) || checker(colTally(xMoves)) || diagonalChecker(xMoves)) {
         document.getElementById('turn').innerHTML=('');
         document.getElementById('turn').innerHTML=('X is the WINNER!')
         return;
@@ -84,7 +90,11 @@ var piece = function(row,sq) {
 var reset = document.querySelector('button');
 button.addEventListener('click', () => {
   var gameSquares = Array.from(document.getElementsByTagName('td'));
-  gameSquares.forEach((item) => item.innerHTML = '')
+  gameSquares.forEach((item) => item.innerHTML = '');
+  document.getElementById('turn').innerHTML=('');
+  document.getElementById('turn').innerHTML=('X\'s turn');
+  xTurn = true;
+  xMoves = [];
+  oMoves = [];
 });
-// keep track of game play and implement rules
 
